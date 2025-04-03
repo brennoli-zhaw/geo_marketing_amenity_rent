2d3aab43e82cd93bb9880173c82add70c968c3ce
brennoli-zhaw <brennoli@students.zhaw.ch>
2025-04-03 20:16:23 +0200

    rental_properties

---
+++ web_scraper.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+import pandas as pd
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+# Initialize the Chrome driver
+driver = webdriver.Chrome(options=chrome_options)
+
+# Navigate to the URL
+
+
+def getPageRentals(pageNr, rental):
+    url = 'https://www.homegate.ch/mieten/immobilien/land-schweiz/trefferliste?ep=' + pageNr
+    driver.get(url)
+    rentalProperties = driver.find_elements(By.CSS_SELECTOR, '[role="listitem"]')
+    if len(rentalProperties) == 0:
+        print("No more rental properties found.")
+        return rental
+    for rentalProperty in rentalProperties:
+        price = rentalProperty.find_element(By.XPATH, "//*[contains(@class, 'HgListingCard_price')]").text
+        address = rentalProperty.find_element(By.TAG_NAME, "address").text
+        rental.append({
+            'address' : address,
+            'price': price,
+        })
+    return rental
+
+maxPages = 50
+rental = []
+for i in range(1, maxPages):
+    pageNr = str(i)
+    print("Scraping page " + pageNr)
+    rental = getPageRentals(pageNr, rental)
+    if i % 10 == 0:
+        print("Sleeping for 5 seconds...")
+        time.sleep(5)
+
+df = pd.DataFrame(rental)
+
+filename = 'rental_properties.json'
+df.to_json(filename, orient='records', lines=True)
+
+#quickfix to save the data in a readable format
+with open(filename, 'w', encoding='utf-8') as file:
+    df.to_json(file, force_ascii=False, orient='records', lines=True)
+print("Data saved to " + filename)
+
+# It's a good practice to close the browser when done
+driver.quit()
